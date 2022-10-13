621df226dc76b29594da5049d5b9e75da464fd43
changsik <lowmans00@gmail.com>
2022-10-13 22:15:32 +0900

    fix: useEffect bug in 18 ver. & add test button

--- basic/src/App.tsx
+++ basic/src/App.tsx
@@ -1,17 +1,20 @@
-import React, { useEffect } from 'react'
-import { EditorState } from 'prosemirror-state'
+import React, { useEffect, useRef } from 'react'
+import { EditorState, Transaction } from 'prosemirror-state'
 import { EditorView } from 'prosemirror-view'
 import { Schema, DOMParser } from 'prosemirror-model'
 import { schema } from './prosemirror/schema-basic'
 import { addListNodes } from 'prosemirror-schema-list'
 import { exampleSetup } from './prosemirror/setup'
-import applyDevTools from "prosemirror-dev-tools"
+import applyDevTools from 'prosemirror-dev-tools'
 
 import './App.css'
 import './Prosemirror.css'
 
 function App() {
+  const viewRef = useRef<EditorView>()
   useEffect(() => {
+    if (viewRef.current) return //FIXME: https://github.com/facebook/react/issues/24502
+
     const mySchema = new Schema({
       nodes: addListNodes(schema.spec.nodes, 'paragraph block*', 'block'),
       marks: schema.spec.marks,
@@ -23,13 +26,19 @@ function App() {
         plugins: exampleSetup({ schema: mySchema }),
       }),
     })
-
-    applyDevTools(view);
-
+    viewRef.current = view
+    applyDevTools(view)
   }, [])
+
+  function testButtonClick() {
+    viewRef?.current?.focus()
+  }
 
   return (
     <div className='App'>
+      <button style={{ marginTop: 16 }} onClick={testButtonClick}>
+        test
+      </button>
       <div style={{ padding: 16 }}>
         <div id='editor'></div>
         <div id='content'></div>
