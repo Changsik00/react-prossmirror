d4a0329d0c83af32587f9a018832520195ef621a
changsik <lowmans00@gmail.com>
2022-11-05 17:08:38 +0900

    fix: exist id checking before make new paragraph id

--- basic/src/prosemirror/setup/create.ts
+++ basic/src/prosemirror/setup/create.ts
@@ -4,22 +4,26 @@ import { nanoid } from 'nanoid'
 import { PID } from '../../constants'
 
 const isTargetNodeOfType = (node: Node, type: NodeType) => node.type === type
-const isNodeHasAttribute = (node: Node, attrName: string) => Boolean(node.attrs && node.attrs[attrName])
-const attrName = PID
+const isNodeHasAttribute = (node: Node, attrName: string) => Boolean(node.attrs && node.attrs.attrName)
 
 export const createPlugin = (uuidGenerator = nanoid) => {
   return new Plugin({
     appendTransaction: (transactions, prevState, nextState) => {
+      // console.log('#@#appendTransaction transactions', transactions)
+      // console.log('#@#appendTransaction prevState', prevState)
+      // console.log('#@#appendTransaction nextState', nextState)
       const tr = nextState.tr
       let modified = false
       if (transactions.some((transaction) => transaction.docChanged)) {
         // Adds a unique id to a node
         nextState.doc.descendants((node, pos) => {
           const { paragraph } = nextState.schema.nodes
-          if (isTargetNodeOfType(node, paragraph) && !isNodeHasAttribute(node, attrName)) {
+          if (isTargetNodeOfType(node, paragraph) && !isNodeHasAttribute(node, 'id')) {
             const attrs = node.attrs
-            tr.setNodeMarkup(pos, undefined, { ...attrs, [attrName]: uuidGenerator() })
-            modified = true
+            if (!attrs.id) {
+              tr.setNodeMarkup(pos, undefined, { ...attrs, id: uuidGenerator() })
+              modified = true
+            }
           }
         })
       }
